9c794c9fa1260ddb153e9ae5e9c1c62865cf0825
AKSHAY BATHLA <AKSHAYBATHLA2005@GMAIL.COM>
2026-04-19 00:06:22 +0530

    Update CORS origin and server log message

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5500"
+    origin: "*"
 }));
 app.use(express.json());
 
@@ -101,5 +101,5 @@ app.use((err, req, res, next) => {
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`GlobalGo Backend running on port ${PORT}`);
 });
